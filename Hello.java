9f888a26d8256849971ef8ed29b2ff4fb0a24963
Peeyush Pandey <peeyushpandey245@gmail.com>
2020-01-28 00:15:08 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,14 @@
 class Test
 {
+    public void solve()
+    {
+        System.out.println("Update 1");
+    }
     public static void main(String[] args)
     {
         System.out.println("Hello World");
         //update 1
         //update 2
+        solve();
     }
 }
